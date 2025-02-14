0aac8df2f084051b54e46566e3293f198b6a15e9
Agustin Conde <conde.agustin66@gmail.com>
2025-02-14 16:17:21 -0300

    Uploads errors

---
+++ src/module/car/error/carError.js
@@ -0,0 +1,3 @@
+module.exports = class CarNotFoundError extends Error {},
+    class CarNotDefinedError extends Error {},
+    class CarIdNotDefinedError extends Error {};
---
+++ src/module/rental/error/RentalError.js
@@ -0,0 +1,3 @@
+module.exports = class RentalNotFoundError extends Error {},
+    class RentalNotDefinedError extends Error {},
+    class RentalIdNotDefinedError extends Error {};
