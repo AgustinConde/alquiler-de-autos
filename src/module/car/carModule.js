6fd56717b62e0da3ab51c2547d138f5b65b1b41c
Agustin Conde <conde.agustin66@gmail.com>
2025-02-14 17:07:13 -0300

    Uploads modules

---
+++ src/module/car/carModule.js
@@ -0,0 +1,18 @@
+const CarController = require('./controller/carController');
+const CarService = require('./service/carService');
+const CarRepository = require('./repository/carRepository');
+const CarModel = require('./model/carModel');
+
+/**
+ * @param {import('express').Application} app
+ * @param {import('rsdi').IDIContainer} container
+ */
+function initCarModule(app, container) {
+  /**
+   * @type {CarController}
+   */
+  const controller = container.get('CarController');
+  controller.configureRoutes(app);
+}
+
+module.exports = { CarController, CarService, CarRepository, CarModel, initCarModule };
