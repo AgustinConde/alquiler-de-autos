2b84f539d939644f8c734e26811e2781cc0ebc25
Agustin Conde <conde.agustin66@gmail.com>
2025-02-21 18:39:10 -0300

    Applies auth logic

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+<<<<<<< Updated upstream
 const sequelize = require("./config/database");
 const CarModel = require("./module/car/model/carModel");
 const ClientModel = require("./module/client/model/clientModel");
@@ -56,3 +57,70 @@ app.listen(PORT, () => {
 });
 
 module.exports = express;
+=======
+const express = require("express");
+const nunjucks = require("nunjucks");
+const path = require("path");
+const session = require("express-session");
+const SQLiteStore = require("connect-sqlite3")(session);
+
+const { sequelizeRental } = require("./config/dbRental");
+const { sequelizeAuth } = require("./config/dbAuth");
+
+
+const RentalModel = require("./module/rental/model/rentalModel");
+const CarModel = require("./module/car/model/carModel");
+const ClientModel = require("./module/client/model/clientModel");
+const AuthModel = require("./module/auth/model/authModel");
+const AuthService = require("./module/auth/service/authService");
+const RentalController = require("./module/rental/controller/rentalController");
+const AuthController = require("./module/auth/controller/authController");
+
+const PORT = 8080;
+const app = express();
+
+app.use(
+  session({
+    store: new SQLiteStore({ db: "sessions.sqlite", dir: "./data" }),
+    secret: process.env.SESSION_PW,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, httpOnly: true },
+  })
+);
+
+CarModel.setup(sequelizeRental);
+ClientModel.setup(sequelizeRental);
+RentalModel.setup(sequelizeRental);
+RentalModel.setAssociations(CarModel, ClientModel);
+AuthModel.setup(sequelizeAuth);
+
+Promise.all([sequelizeRental.sync({ alter: true }), sequelizeAuth.sync({ alter: true })])
+  .then(() => console.log("Databases synchronized successfully"))
+  .catch((error) => console.error("Error synchronizing databases:", error));
+
+nunjucks.configure(path.join(__dirname, "views"), {
+  autoescape: true,
+  express: app,
+  watch: true,
+});
+
+app.set("view engine", "njk");
+app.use(express.static(path.join(__dirname, "../static")));
+app.use("/images", express.static(path.join(__dirname, "../static/images")));
+app.use(express.json());
+
+const authService = new AuthService(AuthModel);
+const authController = new AuthController(authService);
+
+authController.configureRoutes(app);
+
+app.use("/", RentalController);
+app.use("/auth", AuthController); 
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
+>>>>>>> Stashed changes
--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+<<<<<<< Updated upstream
 const { Sequelize } = require("sequelize");
 console.log("Database path:", process.env.DB_PATH);
 
@@ -14,4 +15,24 @@ sequelize
   .then(() => console.log("✅ Connection to database established successfully."))
   .catch((err) => console.error("❌ Unable to connect to the database:", err));
 
+=======
+const { Sequelize } = require('sequelize');
+const ClientModel = require('./models/clientModel');
+const AuthModel = require('./models/authModel');
+const RentalModel = require('./models/rentalModel');
+
+const sequelize = new Sequelize({ 
+    dialect: 'sqlite', 
+    storage: process.env.AUTHDB_PATH 
+});
+
+ClientModel.setup(sequelize);
+AuthModel.setup(sequelize);
+RentalModel.setup(sequelize);
+
+ClientModel.hasOne(AuthModel, { foreignKey: 'clientId', as: 'auth' });
+AuthModel.belongsTo(ClientModel, { foreignKey: 'clientId', as: 'client' });
+
+sequelize.sync();
+>>>>>>> Stashed changes
 module.exports = sequelize;
