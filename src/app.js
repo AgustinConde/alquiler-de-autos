a718d8c2fe17f59cd66b642e74563b6b62639b75
Agustin Conde <conde.agustin66@gmail.com>
2025-02-21 18:42:48 -0300

    Discards old version

--- src/app.js
+++ src/app.js
@@ -1,63 +1,4 @@
 require('dotenv').config();
-<<<<<<< Updated upstream
-const sequelize = require("./config/database");
-const CarModel = require("./module/car/model/carModel");
-const ClientModel = require("./module/client/model/clientModel");
-const RentalModel = require("./module/rental/model/rentalModel");
-const express = require("express");
-const rentalController = require("./module/rental/controller/rentalController");
-const PORT = 8080;
-const nunjucks = require('nunjucks');
-const path = require('path');
-
-const app = express();
-
-const session = require("express-session");
-const SQLiteStore = require("connect-sqlite3")(session);
-
-app.use(session({
-    store: new SQLiteStore({ db: 'sessions.sqlite', dir: './data' }),
-    secret: process.env.SESSION_PW,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false, httpOnly: true }
-}));
-
-
-CarModel.setup(sequelize);
-ClientModel.setup(sequelize);
-RentalModel.setup(sequelize);
-
-RentalModel.setAssociations(CarModel, ClientModel);
-
-
-
-sequelize.sync({ alter:true })
-    .then( () => console.log("Database syncronized succesfully with models"))
-   .catch ((error) => {
-    console.error("Error syncronizing database with models:", error);
-  });
-
-
-nunjucks.configure(path.join(__dirname, 'views'), {
-    autoescape: true,
-    express: app,
-    watch: true,
-});
-app.set('view engine', 'njk');
-app.use(express.static(path.join(__dirname, '../static')));
-app.use('/images', express.static(path.join(__dirname, '../static/images')));
-app.use(express.json());
-
-
-app.use("/", rentalController);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-module.exports = express;
-=======
 const express = require("express");
 const nunjucks = require("nunjucks");
 const path = require("path");
@@ -123,4 +64,3 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
->>>>>>> Stashed changes
--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,4 @@
 require("dotenv").config();
-<<<<<<< Updated upstream
-const { Sequelize } = require("sequelize");
-console.log("Database path:", process.env.DB_PATH);
-
-
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: process.env.DB_PATH,
-  logging: (msg) => console.log(`Sequelize log: ${msg}`)
-});
-
-sequelize
-  .authenticate()
-  .then(() => console.log("✅ Connection to database established successfully."))
-  .catch((err) => console.error("❌ Unable to connect to the database:", err));
-
-=======
 const { Sequelize } = require('sequelize');
 const ClientModel = require('./models/clientModel');
 const AuthModel = require('./models/authModel');
@@ -34,5 +17,4 @@ ClientModel.hasOne(AuthModel, { foreignKey: 'clientId', as: 'auth' });
 AuthModel.belongsTo(ClientModel, { foreignKey: 'clientId', as: 'client' });
 
 sequelize.sync();
->>>>>>> Stashed changes
 module.exports = sequelize;
