5f65a583d721dd4ff3bba6784ae5e10e377621b1
Agustin Conde <conde.agustin66@gmail.com>
2025-02-27 01:53:47 -0300

    Deletes redundant code

--- src/middlewares/authMiddleware.js
+++
@@ -1,8 +0,0 @@
-function isAuthenticated(req, res, next) {
-    if (req.session.user) {
-        return next();
-    }
-    return res.redirect("/login");
-}
-
-module.exports = { isAuthenticated };
