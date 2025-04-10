1c16beb76a44fc290364477b7d961b716e908af0
Agustin Conde <conde.agustin66@gmail.com>
2025-04-09 23:25:02 -0300

    Improves error messages
    Improves error messages giving them custom default values to maintain compatibility and specific class names for better identification

--- src/module/payment/error/paymentError.js
+++ src/module/payment/error/paymentError.js
@@ -1,23 +1,26 @@
 class PaymentNotDefinedError extends Error {
-    constructor() {
-      super('Payment is not defined');
-    }
+  constructor(message = 'Payment is not defined') {
+    super(message);
+    this.name = 'PaymentNotDefinedError';
   }
-  
-  class PaymentIdNotDefinedError extends Error {
-    constructor() {
-      super('Payment ID is not defined');
-    }
+}
+
+class PaymentIdNotDefinedError extends Error {
+  constructor(message = 'Payment ID is not defined') {
+    super(message);
+    this.name = 'PaymentIdNotDefinedError';
   }
-  
-  class PaymentNotFoundError extends Error {
-    constructor(message = 'Payment not found') {
-      super(message);
-    }
+}
+
+class PaymentNotFoundError extends Error {
+  constructor(message = 'Payment not found') {
+    super(message);
+    this.name = 'PaymentNotFoundError';
   }
-  
-  module.exports = {
-    PaymentNotDefinedError,
-    PaymentIdNotDefinedError,
-    PaymentNotFoundError
-  };
+}
+
+module.exports = {
+  PaymentNotDefinedError,
+  PaymentIdNotDefinedError,
+  PaymentNotFoundError
+};
